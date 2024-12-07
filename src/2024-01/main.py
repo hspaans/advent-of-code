871ccd0cb911b46cd5de7cd9df585edc142f7f82
Hans Spaans <1072510+hspaans@users.noreply.github.com>
2024-12-07 12:05:16 +0000

    Add solution for Advent of Code 2024 Day 1 Part Two: calculate similarity score

--- src/2024-01/main.py
+++ src/2024-01/main.py
@@ -74,6 +74,51 @@ is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
 
 Your actual left and right lists contain many location IDs. What is the total
 distance between your lists?
+
+--- Part Two ---
+
+Your analysis only confirmed what everyone feared: the two lists of location
+IDs are indeed very different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read most
+of the Chief's handwriting, but in the commotion you notice an interesting
+detail: a lot of location IDs appear in both lists! Maybe the other numbers
+aren't location IDs at all but rather misinterpreted handwriting.
+
+This time, you'll need to figure out exactly how often each number from the
+left list appears in the right list. Calculate a total similarity score by
+adding up each number in the left list after multiplying it by the number of
+times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+
+For these example lists, here is the process of finding the similarity score:
+
+- The first number in the left list is 3. It appears in the right list three
+  times, so the similarity score increases by 3 * 3 = 9.
+- The second number in the left list is 4. It appears in the right list once,
+  so the similarity score increases by 4 * 1 = 4.
+- The third number in the left list is 2. It does not appear in the right list,
+  so the similarity score does not increase (2 * 0 = 0).
+- The fourth number, 1, also does not appear in the right list.
+- The fifth number, 3, appears in the right list three times; the similarity
+  score increases by 9.
+- The last number, 3, appears in the right list three times; the similarity
+  score again increases by 9.
+
+So, for these example lists, the similarity score at the end of this process
+is 31 (9 + 4 + 0 + 0 + 9 + 9).
+
+Once again consider your left and right lists. What is their similarity score?
 """
 
 
@@ -81,8 +126,8 @@ def main() -> None:
     """
     Historian Hysteria.
 
-    Read the input file and calculate the total distance between the left and
-    right lists. Print the result.
+    Read the input file and calculate the total distance and similarity score
+    between the left and right lists. Print the result.
     """
     with open("input.txt", encoding="UTF-8") as f:
         left = []
@@ -94,7 +139,8 @@ def main() -> None:
         left.sort()
         right.sort()
         distance = sum(abs(a - b) for a, b in zip(left, right))
-        print(distance)
+        similarity = sum(left.count(a) * a for a in right)
+        print(f'Distance bwteen {distance} and similarity score {similarity}')
 
 
 if __name__ == "__main__":
